450c9d08666d1abb58fdbf1289b46aeee25dd11d
Pimenta, Arthur <APimenta@wabtec.com>
2018-09-18 09:20:50 -0300

    creat chat hub and method to send messages

--- server/Startup.cs
+++ server/Startup.cs
@@ -27,6 +27,8 @@ namespace RealTimeChat
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- server/hubs/ChatHub.cs
+++ server/hubs/ChatHub.cs
@@ -8,5 +8,9 @@ namespace RealTimeChat.Hubs
 {
     public class ChatHub: Hub
     {
+        public async Task SendMessage(string user, string message)
+        {
+          await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
     }
 }
