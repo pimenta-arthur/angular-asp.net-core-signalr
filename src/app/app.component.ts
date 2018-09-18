6526d8d0968a9233b8b0570cf369c49cd52830ee
Pimenta, Arthur <APimenta@wabtec.com>
2018-09-18 09:57:37 -0300

    client: try to connect on the signalr hub after angular has initialized

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HubConnection, HubConnectionBuilder } from "@aspnet/signalr";
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-app';
+  _hubConnection: HubConnection;
+  _environment = environment
+
+  ngOnInit(): void {
+    this._hubConnection = new HubConnectionBuilder()
+      .withUrl(this._environment.serverUri + "/chathub")
+      .build();
+
+    this._hubConnection.on("ReceiveMessage", (name: string, msg: string) => {
+    });
+
+    this._hubConnection
+      .start()
+      .then(() => {
+        console.log("Connection started!");
+      })
+      .catch(err => console.log("Error while establishing connection :("));
+  }
+
+  sendMessage(name: string, msg: string)
+  {
+    this._hubConnection.invoke('SendMessage', name, msg)
+      .catch(err => console.log(err));
+  }
 }
